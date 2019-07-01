59608f174654b45863a7c53bd2be9d8e1270fd07
Fabuland <45296816+Fabuland@users.noreply.github.com>
2019-07-01 19:25:18 +0200

    Add files via upload
    Que hace dano a

--- src/main/java/FabuBot.java
+++ src/main/java/FabuBot.java
@@ -59,14 +59,125 @@ public class FabuBot extends TelegramLongPollingBot {
             
         }
 
+        else if(recogWord.toLowerCase().contains("fabu")){
+            message.setText("Espero que hayas dicho algo bueno del todopoderoso Fabuland");
+
+        }
+
         else if(recogWord.toLowerCase().contains("lista de comandos")){
             message.setText("Mis comandos disponibles son:\n" +
                     "- Mejores atacantes\n" +
                     "- Mejores defensores\n" +
                     "- A qué hora empieza el CD\n" +
                     "- Con cuántos cae Groudon?\n" +
+                    "- Qué hace daño a xxxxx (siendo xxxxx un tipo cualquiera)\n" +
                     "\n" +
                     "Se irán actualizando (versión de prueba)");
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a normal")){
+            message.setText("Lucha");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a fuego")){
+            message.setText("Agua, Tierra y Roca");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a agua")){
+            message.setText("Eléctrico y Planta");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a electrico")){
+            message.setText("Tierra");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a eléctrico")){
+            message.setText("Tierra");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a planta")){
+            message.setText("Fuego, Hielo, Veneno, Volador y Bicho");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a hielo")){
+            message.setText("Fuego, Lucha, Roca y Acero");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a lucha")){
+            message.setText("Volador, Psíquico y Hada");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a veneno")){
+            message.setText("Tierra y Psíquico");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a tierra")){
+            message.setText("Agua, Planta y Hielo");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a volador")){
+            message.setText("Eléctrico, Hielo y Roca");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a psiquico")){
+            message.setText("Bicho, Fantasma y Siniestro");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a psíquico")){
+            message.setText("Bicho, Fantasma y Siniestro");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a bicho")){
+            message.setText("Fuego, Volador y Roca");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a roca")){
+            message.setText("Agua, Planta, Lucha y Tierra");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a fantasma")){
+            message.setText("Fantasma y Siniestro");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a dragon")){
+            message.setText("Hielo, Dragon y Hada");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a dragón")){
+            message.setText("Hielo, Dragon y Hada");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a siniestro")){
+            message.setText("Lucha, Bicho y Hada");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a acero")){
+            message.setText("Fuego, Lucha y Tierra");
+
+        }
+
+        else if(recogWord.toLowerCase().contains("daño a hada")){
+            message.setText("Veneno y Acero");
+
         }
 
         else if(recogWord.toLowerCase().contains("/start")){
